0695494ce63d96c014af1b4ef735d6e481515d5c
Michael Dunn <michael@bitnomial.com>
2020-12-13 16:27:16 -0600

    Added more customization.

--- init.el
+++ init.el
@@ -16,33 +16,52 @@
 
 ;; packages
 (use-package solarized-theme :ensure t)
-(use-package company :ensure t)
-(add-hook 'after-init-hook 'global-company-mode) ;; TODO: get this to work as a normal hook
+(use-package company
+  :ensure t
+  :config (global-company-mode))		;; all buffers use company mode
 (use-package helm
   :ensure t
-  :config (helm-mode 1)
+  :config (helm-mode 1)				;; start helm mode automatically
   :bind
-  ("M-x" . helm-M-x)
-  ("C-x C-f" . helm-find-files))
+  ("M-x" . helm-M-x)				;; use helm for all M-x commands
+  ("C-x C-f" . helm-find-files))		;; use helm for finding files
 (use-package flycheck
   :ensure t
-  :config (global-flycheck-mode))
-(use-package ripgrep :ensure t)   ;; ag is inadequate
-(use-package exec-path-from-shell ;; environment variables are the same between emacs and shell
+  :config (global-flycheck-mode))		;; all buffers use flycheck mode
+(use-package ripgrep
   :ensure t
-  :config (exec-path-from-shell-initialize))
+  :bind ("C-c r" . ripgrep-regexp))		;; quick, easy to use keybinding for ripgrep
+(use-package exec-path-from-shell		;; environment variables are the same between emacs and your shell
+  :ensure t
+  :config (exec-path-from-shell-initialize))	;; sets $MANPATH, $PATH, and exec-path from your shell
+(use-package yaml-mode :ensure t)
 
-;; customization
-(load-theme `solarized-dark t)  ;; turn on solarized-dark by default
-(set-default `truncate-lines t) ;; disable line-wrap
-(set-frame-font "Monospace-10") ;; use monospace-10 as default font
-(menu-bar-mode -1)              ;; remove menu bar
-(toggle-scroll-bar -1)          ;; remove scroll bar
-(tool-bar-mode -1)              ;; remove tool bar
-(setq inhibit-startup-screen t) ;; remove startup screen
+;; keyboard customizations
+(setq is-mac (equal system-type 'darwin))	;; sets is-mac to true of macos
+(when is-mac
+  (setq-default
+   mac-command-modifier 'meta			;; changes the meta key from ESC to COMMAND on macos
+   ring-bell-function 'ignore                   ;; turn off bell
+   )
+  )
+(global-set-key (kbd "C-c a") 'align-regexp)	;; bind align-regexp to C-c a
+
+;; completely optional customization
+(load-theme `solarized-dark t)		;; turn on solarized-dark by default
+(set-default `truncate-lines t)		;; disable line-wrap
+(set-frame-font "Monospace-10")		;; use monospace-10 as default font
+(menu-bar-mode -1)			;; remove menu bar
+(toggle-scroll-bar -1)			;; remove scroll bar
+(tool-bar-mode -1)			;; remove tool bar
+(setq inhibit-startup-screen t)		;; remove startup screen
+(global-display-line-numbers-mode)	;; display line numbers on left side
+(show-paren-mode 1)                     ;; highlight matching pairs of parens
+(electric-pair-mode)                    ;; enables auto-pair characters (example, creates groups of brackets automatically)
+
+(add-hook 'before-save-hook
+          'delete-trailing-whitespace)  ;; deletes trailing whitespace on save
 
 ;; custom elisp files
-(add-to-list `load-path "~/.emacs.d/elisp/")
 (load "~/.emacs.d/elisp/haskell-init.el")
 
 ;; ---------------- auto-generated ---------------------
@@ -55,7 +74,16 @@
  '(haskell-tags-on-save t)
  '(package-selected-packages
    (quote
-    (lsp-ui exec-path-from-shell flycheck-haskell ripgrep flycheck helm company use-package haskell-mode solarized-theme))))
+    (yaml-mode lsp-ui exec-path-from-shell flycheck-haskell ripgrep flycheck helm company use-package haskell-mode solarized-theme)))
+ '(safe-local-variable-values
+   (quote
+    ((ormolu-process-path . "fourmolu")
+     (lsp-haskell-formatting-provider . "fourmolu")
+     (haskell-stylish-on-save)
+     (haskell-process-type . stack-ghci)
+     (haskell-indentation-starter-offset . 4)
+     (haskell-indentation-left-offset . 4)
+     (haskell-indentation-layout-offset . 4)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
